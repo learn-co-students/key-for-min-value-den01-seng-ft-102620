160a6c84010f3c37e9258c37183837c3ddd8542a
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-05 19:13:27 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,15 @@
 
 def key_for_min_value(name_hash)
   if name_hash.length == 0 
-      return nil
-    end
-  key = ""
-  currentmin = 999,999,999,999 
-  name_hash.each do |k, v|
-    if v < currentmin 
-      currentmin = v
-      key = k 
-    end
+    return nil 
   end 
-  return key 
+  min_key = ""
+  min_val = 999
+  name_hash.each do |key, value|
+    if value <= min_val
+      min_val = value
+      min_key = key 
+    end
+  end
+  return min_key
 end
